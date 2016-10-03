ae0aecbfa8c2ec10b3f462c8a8404516b3196104
Eric William Blitzer <eblitzer@email.arizona.edu>
2016-10-02 19:56:21 -0700

    Started SectionLeader and OfficeHour classes

--- taqbackend/src/users/SectionLeader.java
+++ taqbackend/src/users/SectionLeader.java
@@ -1,5 +1,20 @@
 package users;
 
 public class SectionLeader {
-
+	
+	private String name;
+	private String netID;
+	private String password;
+	
+	public SectionLeader(String name, String netID, String password){
+		this.name = name;
+		this.netID = netID;
+		this.password = password;
+	}
+	
+	public void changePassword(String newPassword){
+		password = newPassword;
+	}
+	
+	
 }
