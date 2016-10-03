02150b16563dcb64dc4e14190197b3f735d86e54
Eric William Blitzer <eblitzer@email.arizona.edu>
2016-10-02 23:29:36 -0700

    "Run" high level file

--- taqbackend/src/Class.java
+++ taqbackend/src/Class.java
@@ -10,31 +10,7 @@ public class Class {
 	//Notes
 	
 	
- public static void main(String[] whatever){
-	 
-	 Student a = new Student ("jared", "worthington", "jworthington", "password");
-	 Student b = new Student ("Eric", "Blitzer", "eblitzer", "password");
-	 
-	 Class c = new Class( "127A");
-	 
-	 c.addStudent(a);
-	 c.addStudent(b);
-	 
-	 c.studentQueue.openQueue();
-	 
-	 c.studentQueue.addStudent(a);
-	 c.studentQueue.addStudent(b);
-	 
-	 c.studentQueue.deleteStudent(a);
-	 c.studentQueue.addStudent(a);
-	 System.out.println(c.studentQueue.getLength());
-	 
-	 for(int i=0; i<c.studentQueue.getLength(); i++){
-		 System.out.println(c.studentQueue.getStudent(i).get_Student_Name());
-	 }
-	 
-	 
- }
+
  
  
  
