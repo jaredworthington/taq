a6f89d834a405baad5558c91de92d89149b9330d
Eric William Blitzer <eblitzer@email.arizona.edu>
2016-10-02 22:35:02 -0700

    Added "isActive" field to SectionLeader class

--- taqbackend/src/users/SectionLeader.java
+++ taqbackend/src/users/SectionLeader.java
@@ -5,15 +5,25 @@ public class SectionLeader {
 	private String name;
 	private String netID;
 	private String password;
+	private boolean isActive;
 	
 	public SectionLeader(String name, String netID, String password){
 		this.name = name;
 		this.netID = netID;
 		this.password = password;
+		isActive = false;
 	}
 	
 	public void changePassword(String newPassword){
 		password = newPassword;
+	}
+	
+	public void setToActive(){
+		if(isActive==false) isActive=true;
+	}
+	
+	public void setToInactive(){
+		if(isActive==true) isActive=false;
 	}
 	
 	
